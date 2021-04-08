dd6baaf876c1b2ee3d4cfdfc336900918d407fe5
Kieran <kierantmcaleer@gmail.com>
2021-04-08 15:46:19 -0400

    Pulls up the earth quake data and color codes it

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -1,7 +1,7 @@
 // Create a map object
 var myMap = L.map("map", {
     center: [15.5994, -28.6731],
-    zoom: 3
+    zoom: 2
   });
   
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -17,17 +17,31 @@ var myMap = L.map("map", {
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
   for(var i=0; i<data.features.length; i++){
     var mag = data.features[i].properties.mag;
-    var lat = data.features[i].geometry.coordinates[0];
-    var long = data.features[i].geometry.coordinates[1];
     var depth = data.features[i].geometry.coordinates[2]
+    var coordinates = [data.features[i].geometry.coordinates[0], data.features[i].geometry.coordinates[1]]
 
+    if(depth <= 10){
+      color = "#17ff0a"
 
-  //   L.circle(countries[i].location, {
-  //     fillOpacity: 0.75,
-  //     color: "white",
-  //     fillColor: color,
-  //   // Adjust radius
-  //   radius: countries[i].points * 1500
-  // }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
+    } else if(depth >10 && depth <= 30){
+      color= "#5cb60a"
+    } else if(depth >30 && depth <= 50){
+      color= "#f4bd09"
+    }else if(depth >50 && depth <= 70){
+      color= "#f46f0c"
+    }else if(depth >70 && depth <= 90){
+      color= "#ff4702"
+    }
+    else{
+      color="#f90909"
+    }
+
+    L.circle(coordinates, {
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: color,
+    // Adjust radius
+    radius: mag * 15000
+  }).bindPopup("<h1>Magnitude: " + mag + "</h1><hr> <h3>Depth: " + depth + "</h3>").addTo(myMap);
 }
 });
